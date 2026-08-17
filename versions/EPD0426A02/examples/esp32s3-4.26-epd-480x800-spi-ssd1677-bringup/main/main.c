332c6ef898f8b708e29191dd9e0a40b2216e3425
BLSNCD <2857585364@qq.com>
2026-08-17 09:38:03 +0800

    docs(examples): add OSPTEK SPDX header to example entry main.c

--- versions/EPD0426A02/examples/esp32s3-4.26-epd-480x800-spi-ssd1677-bringup/main/main.c
+++ versions/EPD0426A02/examples/esp32s3-4.26-epd-480x800-spi-ssd1677-bringup/main/main.c
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 OSPTEK
+ * SPDX-License-Identifier: CC-BY-4.0
+ *
+ * https://github.com/osptek
+ */
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
